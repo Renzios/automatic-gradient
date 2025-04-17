7a8e8b2fc5f00fa38449bd6b2fa25f6f33104c31
Renzios <61353378+Renzios@users.noreply.github.com.>
2025-04-17 21:46:23 +0800

    feat: add scalar.py

---
+++ scalar.py
@@ -0,0 +1,104 @@
+class Scalar:
+    """
+    A scalar value.
+
+    Attributes:
+        value (float): the value of the scalar
+        operands (set): the operands of the scalar
+        operation (str): the operation of the scalar
+        gradient (float): the gradient of the scalar
+        differentiate (function): the function to differentiate the scalar
+    """
+    def __init__(self, value = 0.0, operands = set(), operation = "", gradient = 0.0, differentiate = lambda: None):
+        """
+        Initializes the Scalar.
+
+        Args:
+            value (float): the value of the scalar
+            operands (set): the operands of the scalar
+            operation (str): the operation of the scalar
+            gradient (float): the gradient of the scalar
+            differentiate (function): the function to differentiate the scalar
+        """
+        self.value = value
+        self.operands = operands
+        self.operation = operation
+        self.gradient = gradient
+        self.differentiate = differentiate
+
+    def __repr__(self):
+        """
+        Returns a string representation of the Scalar.
+        """
+        return f"{self.value}"
+
+    def __add__(self, other):
+        """
+        Returns the sum of two Scalars.
+
+        Args:
+            other (Scalar): the other addend
+
+        Returns:
+            Scalar: the sum of two Scalars
+        """
+        if not isinstance(other, Scalar):
+            other = Scalar(other)
+
+        result = self.value + other.value
+        result = Scalar(result, {self, other}, "+")
+
+        def differentiate():
+            self.gradient += result.gradient
+            other.gradient += result.gradient
+        
+        result.differentiate = differentiate
+
+        return result
+
+    def __mul__(self, other):
+        """
+        Returns the product of two Scalars.
+
+        Args:
+            other (Scalar): the other factor
+
+        Returns:
+            Scalar: the product of two Scalars
+        """
+        if not isinstance(other, Scalar):
+            other = Scalar(other)
+
+        result = self.value * other.value
+        result = Scalar(result, {self, other}, "*")
+
+        def differentiate():
+            self.gradient += result.gradient * other.value
+            other.gradient += result.gradient * self.value
+        
+        result.differentiate = differentiate
+
+        return result
+
+    def __pow__(self, other):
+        """
+        Returns the power of two Scalars.
+
+        Args:
+            other (Scalar): the exponent
+
+        Returns:
+            Scalar: the power of two Scalars
+        """
+        if not isinstance(other, Scalar):
+            other = Scalar(other)
+
+        result = self.value ** other.value
+        result = Scalar(result, {self, other}, "**")
+
+        def differentiate():
+            self.gradient += result.gradient * other.value * self.value ** (other.value - 1)
+        
+        result.differentiate = differentiate
+
+        return result
